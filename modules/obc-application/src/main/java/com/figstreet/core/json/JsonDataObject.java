74c0b08a6cf810b5db46543386516bbd6dd2b204
Jonathan <74478108+figstreet@users.noreply.github.com>
2024-02-20 05:42:20 -0500

    Comment-out System.out calls

--- modules/obc-application/src/main/java/com/figstreet/core/json/JsonDataObject.java
+++ modules/obc-application/src/main/java/com/figstreet/core/json/JsonDataObject.java
@@ -153,9 +153,9 @@ public abstract class JsonDataObject implements Jsonable, Serializable
 		BufferedReader reader = null;
 		try
 		{
-			System.out.println("it is executing 1 " + pJsonFile);
+			//System.out.println("it is executing 1 " + pJsonFile);
 			reader = Files.newBufferedReader(pJsonFile);
-			System.out.println("it is executing 2");
+			//System.out.println("it is executing 2");
 			jsonData = pGson.fromJson(reader, pClass);
 		}
 		finally
