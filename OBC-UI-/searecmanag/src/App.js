07a7e56f1385ada4a889079dc125fc31a287a769
CharleHei <94128650+CharleHei@users.noreply.github.com>
2024-04-23 00:39:51 +0300

    Add files via upload

---
+++ OBC-UI-/searecmanag/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import { LoginSingup } from './compaonents/loginsignup/LoginSingup';
+
+function App() {
+  return (
+    <div >
+    <LoginSingup/>
+    </div>
+  );
+}
+
+export default App;
