acbb019aa90ecb616f9a59cbfcd7c9fd3a1da9f1
Jonathan Schwehm <jonathan@figstreet.com>
2024-07-27 07:38:25 -0400

    Modify reference to LoginSignup component to work on Windows.

--- modules/obc-ui/src/App.js
+++ modules/obc-ui/src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import LoginSignup from './components/loginsignup/LoginSingup';
+import LoginSignup from './components/LoginSignup/LoginSignup.jsx';
 
 
 
